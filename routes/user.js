7cb2b1f8f02d9ed004279abfb18f0efb5582328c
Sandeep K A <119618083+Sandeep-K-A@users.noreply.github.com>
2023-06-05 00:29:54 +0530

    updates user routes

--- routes/user.js
+++ routes/user.js
@@ -7,19 +7,8 @@ const {processCoupon} = require('../controller/coupon-controller')
 const {isLoggedIn,isBlocked} = require('../middleware/middleware')
 
 
-/* GET users listing. */
+/*******************************User Signup and Signin routes*******************************/
 
-// router.use((req,res,next)=>{
-//     var cartCount = null;
-//     if(req.session.user){
-//         const id = req.session.user._id;
-//         getcartCount(id).then((count)=>{
-//             cartCount = count;
-//         })
-//     }
-//     console.log(cartCount,'UUUUUUUUUUUUUUUUUUUUU')
-//     next();
-// })
 router.get('/',getHome)
 router.get('/logout',logOut)
 router.get('/signup',getSignup)
@@ -29,31 +18,42 @@ router.post('/signin',postSignin)
 router.get('/otpsignin',Otpsignin)
 router.post('/otpsignin',getOtp)
 router.post('/otpverify',OtpLogin)
+
+/*******************************User Shop and Shop routes*******************************/
+
 router.get('/shop',isBlocked,getShop)
 router.post('/search/:key',getSearchProducts)
 router.get('/quickview/:id',productView)
-router.get('/account',isBlocked,isLoggedIn,getAccount)
+
+/*******************************User cart and cart routes*******************************/
+
 router.get('/cart',isBlocked,isLoggedIn,getCart)
 router.get('/add-to-cart/:id',isBlocked,isLoggedIn,addToCart)
 router.post('/change-product-quantity',isBlocked,isLoggedIn,updateQuantity)
+router.post('/remove-single-product',isBlocked,isLoggedIn,removeProduct)
+router.get('/clear-cart/:id',isBlocked,isLoggedIn,clearCart)
+
+/*******************************User Order and Order routes*******************************/
+
+router.get('/checkout',isBlocked,isLoggedIn,getCheckOut)
+router.post('/apply-coupon/:id',isBlocked,isLoggedIn,processCoupon)
+router.get('/apply-coupon/:id',isBlocked,isLoggedIn,processCoupon)
+router.post('/placeorder',isBlocked,isLoggedIn,placeOrder)
+router.post('/verify-payment',isBlocked,isLoggedIn,verifyPayment)
+router.get('/order-placed',isBlocked,isLoggedIn,orderPlaced)
+
+
+/*******************************User Account and Account routes*******************************/
+router.get('/account',isBlocked,isLoggedIn,getAccount)
 router.get('/add-address',isBlocked,isLoggedIn,getAddaddress)
 router.post('/add-address',isBlocked,isLoggedIn,newAddress)
-router.get('/checkout',isBlocked,isLoggedIn,getCheckOut)
-router.post('/placeorder',isBlocked,isLoggedIn,placeOrder)
-router.get('/order-placed',isBlocked,isLoggedIn,orderPlaced)
 router.get('/allorders',isBlocked,isLoggedIn,viewOrders)
 router.get('/cancel-order/:id',isBlocked,isLoggedIn,cancelOrder)
 router.get('/return-order/:id',isBlocked,isLoggedIn,returnOrder)
 router.get('/orderdetails/:id',isBlocked,isLoggedIn,orderDetails)
 router.get('/invoice/:id',isBlocked,isLoggedIn,getInvoice)
-router.post('/remove-single-product',isBlocked,isLoggedIn,removeProduct)
-router.get('/clear-cart/:id',isBlocked,isLoggedIn,clearCart)
-// router.get('/manage-address',displayAllAddress)
-router.post('/verify-payment',isBlocked,isLoggedIn,verifyPayment)
 router.post('/edit-address',isBlocked,isLoggedIn,editAddress)
 router.post('/update-account',isBlocked,isLoggedIn,updateUserDetails)
-router.post('/apply-coupon/:id',isBlocked,isLoggedIn,processCoupon)
-router.get('/apply-coupon/:id',isBlocked,isLoggedIn,processCoupon)
 router.get('/forgot-password',getForgotPassword)
 router.post('/forgot-password',postForgotPassword)
 router.post('/otp-forgotpassword',passwordChange)
